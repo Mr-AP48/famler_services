a77ddd0e80ab872adc3636f39f677d66440e464a
Mr AP <83350206+Mr-AP48@users.noreply.github.com>
2021-07-14 10:46:41 -0700

    Add files via upload

---
+++ services.js
@@ -0,0 +1,11 @@
+function aboutUs () {
+    window.location = "about_us.html";
+}
+
+function services () {
+    window.location = "services.html"
+}
+
+function contactUs () {
+    window.location = "contact_us.html"
+}
